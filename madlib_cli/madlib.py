08b7a3656e068b7ec38c912dabc1b331ca04a405
Dario Thornhill <dario.thornhill@gmail.com>
2021-05-09 14:40:09 +0300

    added parse function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,10 +1,15 @@
+import re
+
 def read_template(path):
     with open(path, 'r') as file:
         raw_contents = file.read()
     return raw_contents
 
-def parse_template():
-    pass
+def parse_template(template):
+    pattern = r"{(.*?)}"
+    parts = re.findall(pattern, template)
+    stripped = re.sub(pattern, "{}", template)
+    return [stripped, parts]
 
 def merge():
     pass
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -7,22 +7,40 @@ def test_read_template_returns_stripped_string():
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
+
 # @pytest.mark.skip
 def test_read_template_returns_stripped_string_multi():
     actual = read_template("assets/dark_and_stormy_night_multi.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}.\nIt was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
-@pytest.mark.skip
-def test_parse_template():
-    actual_stripped, actual_parts = parse_template(
-        "It was a {Adjective} and {Adjective} {Noun}."
-    )
-    expected_stripped = "It was a {} and {} {}."
-    expected_parts = ("Adjective", "Adjective", "Noun")
 
-    assert actual_stripped == expected_stripped
-    assert actual_parts == expected_parts
+# @pytest.mark.skip
+def test_parse_template(templates):
+    for template in templates:
+        actual_stripped, actual_parts = parse_template(template["template"])
+        expected_stripped = template["stripped"]
+        expected_parts = template["parts"]
+
+        assert actual_stripped == expected_stripped
+        assert actual_parts == expected_parts
+
+
+@pytest.fixture
+def templates():
+    return [
+        {
+            "template": "It was a {Adjective} and {Adjective} {Noun}.",
+            "stripped": "It was a {} and {} {}.",
+            "parts": ["Adjective", "Adjective", "Noun"],
+        },
+        {
+            "template": "Give me a {Adjective} {Noun} to {Verb}.",
+            "stripped": "Give me a {} {} to {}.",
+            "parts": ["Adjective", "Noun", "Verb"],
+        },
+    ]
+
 
 @pytest.mark.skip
 def test_merge():
@@ -30,10 +48,10 @@ def test_merge():
     expected = "It was a dark and stormy night."
     assert actual == expected
 
+
 @pytest.mark.skip
 def test_read_template_raises_exception_with_bad_path():
 
     with pytest.raises(FileNotFoundError):
         path = "missing.txt"
         read_template(path)
-
